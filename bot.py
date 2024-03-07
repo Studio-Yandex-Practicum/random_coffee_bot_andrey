b4449abf24be76f71f8bd0fba375a19d7b1d10db
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2024-03-07 19:11:11 +0300

    start_bot

--- bot.py
+++ bot.py
@@ -1,9 +1,30 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_handler(message: types.Message) -> None:
+    await message.answer('Старт бота')
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    await message.answer(message.text)
 
 
 async def main():
-    pass
+    await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    asyncio.run(main())
 
