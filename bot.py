400e9555e44a1d60f2a7985411d67cb272dbfb84
aten88 <virtuoso_ten@mail.ru>
2024-03-07 11:49:30 +0300

    Создал бота и научил его здороваться

--- bot.py
+++ bot.py
@@ -1,8 +1,26 @@
+import os
+import asyncio
+import logging
 
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=os.getenv('BOT_TOKEN'))
+
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def bot_start(message: types.Message):
+    await message.answer("Кофебот приветствует тебя!")
 
 async def main():
-    pass
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    pass
+    asyncio.run(main())
