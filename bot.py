fa2da0da41cba859e18ba556bceaa98a539cbf7b
Maksim Boiko <max.asus1742@gmail.com>
2024-03-08 10:23:32 +0300

    Собрал основу для бота

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
+import asyncio
+
+from tg_bot.handlers.default import default_router
+from tg_bot.loader import bot, dp
 
 
 async def main():
-    pass
+    try:
+        dp.include_router(default_router)
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 
 if __name__ == "__main__":
-    pass
-
+    asyncio.run(main())
