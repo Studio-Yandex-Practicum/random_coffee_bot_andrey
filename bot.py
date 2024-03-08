a8984f7d80d1ffc637ca6744572e24e5a461d4ea
Константин Стеблев <konstantin.sks85@gmail.com>
2024-03-08 15:13:37 +0100

    before_redis_install

--- bot.py
+++ bot.py
@@ -1,9 +1,54 @@
+import asyncio
+# import logging
+import os
+
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.filters.command import Command
+from dotenv import load_dotenv
+
+# from tg_bot.config import logger
+
+load_dotenv()
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+# logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=os.getenv('BOT_TOKEN'))  # "12345678:AaBbCcDdEeFfGgHh")
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command('start'))
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text='Да, хочу!'),
+            types.KeyboardButton(text='Кофе для слабаков! Только минералка!')
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder='Хотите провести встречу за чашечкой кофе?'
+    )
+    await message.answer('Дратуйте', reply_markup=keyboard)
+#    await message.answer('Дратуйте!')
+
+
+@dp.message(F.text.lower() == 'да, хочу!')
+async def with_puree(message: types.Message):
+    await message.reply('Держи кофе')
+
+
+@dp.message(F.text.lower() == 'кофе для слабаков! только минералка!')
+async def without_puree(message: types.Message):
+    await message.answer('Здесь только кофе!')
+# Запуск процесса поллинга новых апдейтов\
 
 
 async def main():
-    pass
+    await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
-    pass
-
+if __name__ == '__main__':
+    asyncio.run(main())
--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,21 +1,45 @@
-from environs import Env
+import logging
+import os
+import sys
 
-from tg_bot.logs.logs_setup import loggers
+from dotenv import load_dotenv
+from logging import StreamHandler
 
-
-env = Env()
-env.read_env()
-
-"""Tokens"""
-BOT_TOKEN = env.str('BOT_TOKEN')
+load_dotenv()
 
 """Loggers"""
-bot_logger = loggers['BotLogger']
+logging.basicConfig(
+    level=os.getenv('LOGGING_LEVEL'),  # переменная LOGGING_LEVEL в .env
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+# Далее подготовлен логер для возможности применения к разным хэндлерам бота.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+handler = StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+"""Tokens"""
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+
+# env = Env()
+# env.read_env()
+
+"""Tokens"""
+# BOT_TOKEN = env.str('BOT_TOKEN')
+
+"""Loggers"""
+# bot_logger = loggers['BotLogger']
 
 """Redis"""
-redis_host = env.str('REDIS_HOST', None)
-redis_port = env.str('REDIS_PORT', None)
+# redis_host = env.str('REDIS_HOST', None)
+# redis_port = env.str('REDIS_PORT', None)
 
 """Django"""
-super_user_name = env.str('SUPER_USER_NAME')
-super_user_pass = env.str('SUPER_USER_PASS')
+# super_user_name = env.str('SUPER_USER_NAME')
+# super_user_pass = env.str('SUPER_USER_PASS')
