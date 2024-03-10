462ad071588c906fb0ed5833bb36d88f2f187a38
Maksim Boiko <max.asus1742@gmail.com>
2024-03-10 13:58:48 +0300

    Применил State при вводе имя, фамилии

--- tg_bot/handlers/default.py
+++ tg_bot/handlers/default.py
@@ -1,6 +1,9 @@
 from aiogram import Router
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+
+from tg_bot.states.all_states import StepsRegister
 
 default_router = Router()
 
@@ -9,3 +12,24 @@ default_router = Router()
 async def bot_start(message: Message):
     """Ввод команды /start"""
     await message.answer("Кофе-бот приветствует тебя!")
+
+
+@default_router.message(Command('name'))
+async def command_name(message: Message, state: FSMContext):
+    """Ввод команды /name"""
+    await message.answer('Введите свои имя и фамилию')
+    await state.set_state(StepsRegister.GET_NAME)
+
+
+@default_router.message(StepsRegister.GET_NAME)
+async def get_name(message: Message):
+    """Получение имени"""
+    name_parts = message.text.strip().split(' ')
+
+    if len(name_parts) != 2 or not all(part.isalpha() for part in name_parts):
+        await message.answer('Введите, свои имя и фамилию, состоящие только '
+                             'из букв и разделенные пробелом.')
+        return
+
+    full_name = ' '.join(part.capitalize() for part in name_parts)
+    await message.answer(f'Вас зовут {full_name}')
