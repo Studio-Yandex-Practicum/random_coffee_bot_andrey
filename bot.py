8d784f5bcaca5c297062cc6f01f4af61ff0e97a2
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2024-03-10 14:47:47 +0300

    fix cmd /admin

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from tg_bot.loader import bot, dp
 async def main():
     logging.info('Начало работы бота.')
     try:
-        await bot.delete_webhook(drop_pending_updates=True)
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
--- tg_bot/handlers/admin.py
+++ tg_bot/handlers/admin.py
@@ -1,5 +1,5 @@
 from aiogram import types, Router, F
-from aiogram.filters import Command, StateFilter
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 
