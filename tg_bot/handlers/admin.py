eab48cd9211d3e5583b1be09958155e3ba0c236d
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2024-03-10 15:11:48 +0300

    fix cmd /admin

--- tg_bot/handlers/admin.py
+++ tg_bot/handlers/admin.py
@@ -1,6 +1,7 @@
-from aiogram import types, Router, F
+from aiogram import types, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
 
 from tg_bot.states.all_states import Admin
 
@@ -8,30 +9,21 @@ admin_router = Router()
 
 
 @admin_router.message(Command('admin'))
-async def admin_message(message: types.Message, state: FSMContext):
+async def admin_message(message: Message, state: FSMContext):
     """Ввести фамилию"""
-    await message.answer('Введите фамилию:')
-    await state.set_state(Admin.last_name)
+    await message.answer('Введите свои имя и фамилию:')
+    await state.set_state(Admin.get_name)
 
 
-@admin_router.message(Admin.last_name, F.text)
-async def admin_message(message: types.Message, state: FSMContext):
-    """Ввести имя"""
-    if message.text.isalpha():
-        await state.update_data(last_name=message.text.capitalize())
-        await message.answer('Введите имя:')
-        await state.set_state(Admin.first_name)
-    else:
-        await message.answer('Фамилия должна состоять только из букв')
+@admin_router.message(Admin.get_name)
+async def get_name(message: Message):
+    """Получение имени и фамилии"""
+    name_parts = message.text.strip().split(' ')
 
+    if len(name_parts) != 2 or not all(part.isalpha() for part in name_parts):
+        await message.answer('Введите, свои имя и фамилию, состоящие только '
+                             'из букв и разделенные пробелом.')
+        return
 
-@admin_router.message(Admin.first_name, F.text)
-async def admin_message(message: types.Message, state: FSMContext):
-    """Вывод Фамилии и имени пользователю"""
-    if message.text.isalpha():
-        await state.update_data(first_name=message.text.capitalize())
-        data = await state.get_data()
-        await message.answer(f"Администратор: {data['first_name']} {data['last_name']} зарегистрирован.")
-        await state.clear()
-    else:
-        await message.answer('Имя должно состоять только из букв')
+    full_name = ' '.join(part.capitalize() for part in name_parts)
+    await message.answer(f'{full_name}')
