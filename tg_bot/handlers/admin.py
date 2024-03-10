02a125b04073131797f09c6749ecc937613df33f
Ilya Fabiyanskiy <ilya.fabiyanskiy@yandex.ru>
2024-03-10 14:57:56 +0300

    fix cmd /admin

--- tg_bot/handlers/admin.py
+++ tg_bot/handlers/admin.py
@@ -1,35 +1,31 @@
 from aiogram import types, Router, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
+
+from tg_bot.states.all_states import Admin
 
 admin_router = Router()
-
-
-class AdminRegistration(StatesGroup):
-    last_name = State()
-    first_name = State()
 
 
 @admin_router.message(Command('admin'))
 async def admin_message(message: types.Message, state: FSMContext):
     """Ввести фамилию"""
     await message.answer('Введите фамилию:')
-    await state.set_state(AdminRegistration.last_name)
+    await state.set_state(Admin.last_name)
 
 
-@admin_router.message(AdminRegistration.last_name, F.text)
+@admin_router.message(Admin.last_name, F.text)
 async def admin_message(message: types.Message, state: FSMContext):
     """Ввести имя"""
     if message.text.isalpha():
         await state.update_data(last_name=message.text.capitalize())
         await message.answer('Введите имя:')
-        await state.set_state(AdminRegistration.first_name)
+        await state.set_state(Admin.first_name)
     else:
         await message.answer('Фамилия должна состоять только из букв')
 
 
-@admin_router.message(AdminRegistration.first_name, F.text)
+@admin_router.message(Admin.first_name, F.text)
 async def admin_message(message: types.Message, state: FSMContext):
     """Вывод Фамилии и имени пользователю"""
     if message.text.isalpha():
--- tg_bot/states/all_states.py
+++ tg_bot/states/all_states.py
@@ -1,1 +1,5 @@
+from aiogram.fsm.state import State, StatesGroup
 
+
+class Admin(StatesGroup):
+    get_name = State()
