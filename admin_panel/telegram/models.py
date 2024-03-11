88886bc349d3718172c944f31e6a7c03d6c33b5b
Maksim Boiko <max.asus1742@gmail.com>
2024-03-11 14:06:42 +0300

    Написал модель юзера

--- admin_panel/telegram/models.py
+++ admin_panel/telegram/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class TgUser(models.Model):
+    """Модель пользователя"""
+    id = models.BigIntegerField(unique=True)
+    email = models.EmailField(null=True, blank=True)
+    enter_full_name = models.CharField(max_length=100, null=True, blank=True)
+    username = models.CharField(max_length=32, null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    bot_unblocked = models.BooleanField(default=True)
+    is_unblocked = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return f"{self.full_name} ({self.email})"
