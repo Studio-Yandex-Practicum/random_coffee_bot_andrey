79d3ecf83cd7b60e0fbd66bb31d42e3e0eaf227f
Хлестов Андрей <andrei-h-74@mail.ru>
2024-03-12 13:06:37 +0500

    fix db

--- admin_panel/telegram/models.py
+++ admin_panel/telegram/models.py
@@ -3,26 +3,32 @@ from django.db import models
 
 class TgUser(models.Model):
     """Модель пользователя"""
-    id = models.BigIntegerField(unique=True)
-    email = models.EmailField('Почта', null=True, blank=True)
-    enter_full_name = models.CharField('Введенное пользователем имя',
-                                       max_length=100,
-                                       null=True,
-                                       blank=True)
-    username = models.CharField('Имя пользователя',
-                                max_length=32,
-                                null=True,
-                                blank=True)
-    full_name = models.CharField('Полное имя', max_length=100)
-    bot_unblocked = models.BooleanField('Бот разблокирован пользователем',
-                                        default=True)
-    is_unblocked = models.BooleanField('Пользователь разблокирован',
-                                       default=True)
-    is_admin = models.BooleanField('Права администратора', default=False)
+    id = models.BigIntegerField(
+        verbose_name='ID пользователя в Telegram', primary_key=True)
+    email = models.EmailField(verbose_name='Почта', null=True, blank=True)
+    enter_full_name = models.CharField(
+        verbose_name='Введенное пользователем имя и фамилия',
+        max_length=100,
+        null=True,
+        blank=True
+    )
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=32,
+        null=True,
+        blank=True
+    )
+    full_name = models.CharField(verbose_name='Полное имя', max_length=100)
+    bot_unblocked = models.BooleanField(
+        verbose_name='Бот разблокирован пользователем', default=True)
+    is_unblocked = models.BooleanField(
+        verbose_name='Пользователь разблокирован', default=True)
+    is_admin = models.BooleanField(
+        verbose_name='Права администратора', default=False)
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        f'{self.id} {self.enter_full_name}'
+        return f'{self.id} {self.enter_full_name}'
