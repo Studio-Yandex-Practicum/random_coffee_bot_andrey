a39f4e561e14f7eefa841f8969ae66a17c06461a
aten88 <virtuoso_ten@mail.ru>
2024-03-13 18:45:06 +0300

    Вынес текст в отдельную переменную

--- tg_bot/handlers/main_menu.py
+++ tg_bot/handlers/main_menu.py
@@ -4,11 +4,7 @@ from aiogram.types import Message
 
 main_menu_router = Router()
 
-
-@main_menu_router.message(F.text == 'О проекте')
-async def about_project(message: Message):
-    """ Раздел о проекте """
-    await message.answer('''
+ABOUT_TEXT = '''
 В нашей компании есть прекрасная традиция знакомиться за чашечкой горячего кофе в Microsoft Teams или в офисе.
 Раз в неделю будет автоматически приходить имя и фамилия коллеги в этот чат-бот, вам остается договориться
 о дате и времени встречи в удобном для вас формате.
@@ -17,4 +13,10 @@ async def about_project(message: Message):
     - Приятный и неожиданный сюрприз;
     - Помощь новым коллегам в адаптации;
     - Новые знакомства.
-''') # noqa
+''' # noqa
+
+
+@main_menu_router.message(F.text == 'О проекте')
+async def about_project(message: Message):
+    """ Раздел о проекте """
+    await message.answer(ABOUT_TEXT)
