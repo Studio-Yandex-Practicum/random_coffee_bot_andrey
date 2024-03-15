b7b24a5189c27fced00dfbf87c5c71d3bb579b08
aten88 <virtuoso_ten@mail.ru>
2024-03-15 18:07:48 +0300

    Создал Оркестр Контейнеров| Не удалось подключить статику только

--- admin_panel/django_settings/settings.py
+++ admin_panel/django_settings/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
