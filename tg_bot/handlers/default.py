b3fae5c1556526a14e33f3f8bfc1a91ccd746545
Хлестов Андрей <andrei-h-74@mail.ru>
2024-03-20 22:47:43 +0500

    add create test users

--- tg_bot/handlers/default.py
+++ tg_bot/handlers/default.py
@@ -23,3 +23,32 @@ async def command_start(message: Message, state: FSMContext):
         await main_menu(message)
     else:
         await start_registration(message, state)
+
+
+@default_router.message(Command('test'))
+async def command_test(message: Message, state: FSMContext):
+    from tg_bot.misc.creating_unique_pairs import generate_unique_pairs
+    from admin_panel.telegram.models import TgUser
+    for i in range(1, 6):
+        username = f"user{i}"
+        email = f"user{i}@groupeseb.ru"
+        full_name = f"User {i} Full Name"
+        enter_full_name = f"User {i} Enter Full Name"
+
+        # Проверяем существование пользователя с таким email
+        if TgUser.objects.filter(email=email).exists():
+            print(
+                f"Пользователь с адресом {email} уже существует, пропускаем создание.")
+            continue
+
+        # Создаем пользователя в базе данных
+        user = TgUser.objects.create(
+            id=i,
+            email=email,
+            enter_full_name=enter_full_name,
+            username=username,
+            full_name=full_name
+        )
+        print(f"Создан пользователь: {user.email}")
+
+    generate_unique_pairs()
